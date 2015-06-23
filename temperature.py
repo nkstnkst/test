47a3c0767352c74f81d97017d6e601c7145cb0b5
Chang <cliu18@ncsu.edu>
2015-06-23 09:54:54 -0400

    add k2c and f2c function

--- temperature.py
+++ temperature.py
@@ -9,4 +9,11 @@ def f_to_k(ftemp):
     converted = ((ftemp - 32.) * ( 5./9.)) +273.15
     return converted
 
-print(f_to_k(100.))
+
+def k_to_c(ktemp):
+    return ktemp - 273.15
+
+def f_to_c(ftemp):
+    return k_to_c(f_to_k(ftemp))
+
+print(f_to_c(100.))
